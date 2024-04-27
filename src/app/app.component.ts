f3e762ff97875d9bf9c20de87faa5f1c0d1d7774
Robin31 <robindenis@hotmail.fr>
2024-04-27 16:27:34 +0200

    coucou

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Bonjour';
 
-  isAdmin: boolean = true;
+  isAdmin: boolean = false;
+  admin(): boolean {
+    return this.isAdmin = !this.isAdmin;
+  }
 }
